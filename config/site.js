21374190129fb7b66058665377203d8300c873a9
Frederick D. Dineen <19985146+donwolfonline@users.noreply.github.com>
2024-12-14 23:03:05 +0300

    Initial commit with clean history

---
+++ config/site.js
@@ -0,0 +1,20 @@
+export const siteConfig = {
+  showName: true,
+  name: 'iLibre.',
+  ghost: `bg-transparent p-2 flex space-x-2 items-center hover:bg-slate-100 rounded-xl`,
+  description: ' Open Source Linktree Alternative for developers',
+  mainNav: [],
+  links: {
+    twitter: 'https://twitter.com/vivoapps',
+    github: 'https://github.com/donwolfonline/iLibre',
+  },
+  redirects: {
+    twitter:
+      'https://twitter.com/intent/tweet?text=iLibre%20is%20seriously%20impressive!%20%F0%9F%91%8F%20%0AI%20was%20able%20to%20create%20my%20link%20in%20bio%20page%20in%20just%20minutes.%0A%20%0AA%20huge%20shoutout%20to%20%40vivoapps%20for%20creating%20this%20platform.%20%0A%0ACheck%20it%20out%3A%20https%3A//ilibre.vercel.app/%F0%9F%9A%80%0A%0A%23ilibre%20%20%20%20',
+    instagram: 'https://instagram.com/',
+    linkedin:
+      'https://www.linkedin.com/shareArticle?mini=true&url=https%3A//ilibre.vercel.app/',
+  },
+  themeTagline:
+    'Personalize your iLibre profile by modifying the background with colors, gradients, etc. Select a button design, and explore additional customization options.',
+};
